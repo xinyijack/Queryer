c1a1382e3783b570bee50f5ce8ab6e9ac81bf095
gitee <xinyijack@qq.com>
2024-03-14 16:32:57 +0800

    pub use dialect::example_sql; pub use dialect::TryDialect; queryer中的lib.rs缺少这两行，导致queryer-py中的lib.rs无法直接Ok(queryer::example_sql())调用，加入这两行已修复

--- queryer-py/src/lib.rs
+++ queryer-py/src/lib.rs
@@ -1,8 +1,31 @@
 #![allow(clippy::needless_option_as_deref)]
 use pyo3::prelude::pyfunction;
-use pyo3::PyResult;
+use pyo3::{exceptions, PyResult, Python, wrap_pyfunction};
+use pyo3::proc_macro::pymodule;
+use pyo3::types::PyModule;
 
 #[pyfunction]
 pub fn example_sql() -> PyResult<String> {
     Ok(queryer::example_sql())
+}
+
+#[pyfunction]
+pub fn query(sql: &str, output: Option<&str>) -> PyResult<String> {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let data = rt.block_on(async { queryer::query(sql).await.unwrap() });
+    match output {
+        Some("csv") | None => Ok(data.to_csv().unwrap()),
+        Some(v) => Err(exceptions::PyTypeError::new_err(format!(
+            "Output type {} not supported",
+            v
+        ))),
+    }
+}
+
+#[pymodule]
+fn queryer_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(query, m)?)?;
+    m.add_function(wrap_pyfunction!(example_sql, m)?)?;
+
+    Ok(())
 }
